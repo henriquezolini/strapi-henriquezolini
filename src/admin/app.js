c873f34e713314bce2450f89952610f6f3977f66
Henrique Zolini <henriquezolini@gmail.com>
2023-04-14 19:21:12 -0300

    adiciona modulos post e experiencia

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,5 +1,21 @@
 const config = {
   locales: ["pt-BR", "en"],
+  tutorials: false,
+  notifications: {
+    releases: false,
+  },
+  translations: {
+    en: {
+      "app.components.LeftMenu.navbrand.title": "Dashboard",
+      "Auth.form.welcome.subtitle": "Login to your account",
+      "Auth.form.welcome.title": "Welcome",
+    },
+    "pt-BR": {
+      "app.components.LeftMenu.navbrand.title": "Painel",
+      "Auth.form.welcome.subtitle": "Entrar na sua conta",
+      "Auth.form.welcome.title": "Bem-vindo(a)!",
+    },
+  },
 };
 
 const bootstrap = () => {};
